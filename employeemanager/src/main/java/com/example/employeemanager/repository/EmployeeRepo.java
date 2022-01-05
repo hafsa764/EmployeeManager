27a34493f7fe0376c627e526501ae545eb93b31e
kh-hafsa <72071540+hafsa764@users.noreply.github.com>
2022-01-05 22:51:35 +0100

    Add files via upload

---
+++ employeemanager/src/main/java/com/example/employeemanager/repository/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.example.employeemanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
